f6e21d05c180958cdfe649b8225578fc90fe786b
Bartlomiej Szala <bs@vedevo.io>
2020-10-26 18:53:35 +0100

    wip

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -256,6 +256,7 @@
                 if(!status){
                     if(!this.finished){
                         this.init();
+                        BodyTypeManager.releasePause();
                     }
                 }
             },
